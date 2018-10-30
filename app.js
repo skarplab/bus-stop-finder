cb4c8acc779dd02e0feed37bf68f2b37762aa197
Ryan Cooper <ryancooper729@gmail.com>
2018-10-30 14:55:27 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let map = L.map('map').setView([35.779591, -78.638176], 15);
+
+let basemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
